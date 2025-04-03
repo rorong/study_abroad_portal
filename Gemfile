315d864e93187a35156e036f2e3c21207ccfd9fc
rordev <rordev@ongraph.com>
2025-04-03 18:51:00 +0530

    added css

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ source "https://rubygems.org"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 8.0.0"
 # The modern asset pipeline for Rails [https://github.com/rails/propshaft]
-gem "propshaft"
 # Use sqlite3 as the database for Active Record
 gem "pg"
 # Use the Puma web server [https://github.com/puma/puma]
@@ -75,3 +74,4 @@ gem 'rails_admin'
 gem "cssbundling-rails"
 gem 'sassc-rails'
 gem 'sass-embedded'
+gem "sprockets-rails"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,10 +25,11 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  config.assume_ssl = true
+  config.assume_ssl = false
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
+
   config.active_record.cache_versioning = false
 
   # Skip http-to-https redirect for the default health check endpoint.
